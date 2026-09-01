c4e3feed11e260bce92be654e00c87e496ff5925
danabala72 <danabala72@gmail.com>
2026-09-01 21:41:17 +0800

    feat: add protected layout

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,4 @@
+import type { ReactNode } from "react";
+import { AppShell } from "@/components/app-shell";
+import { requireUser } from "@/lib/auth";
+export default async function ProtectedLayout({ children }: { children: ReactNode }) { const user = await requireUser(); return <AppShell username={user.username} role={user.role}>{children}</AppShell>; }
