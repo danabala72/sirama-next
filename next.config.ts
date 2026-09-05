c3d339a52507f398f80250669a6d76b30048eb40
danabala72 <danabala72@gmail.com>
2026-09-05 15:13:02 +0800

    Refactor next.config.ts to use TypeScript types

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'standalone',
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'sirama.pnb.ac.id',
+        'abdiserver.tailee8978.ts.net',
+      ],
+    },
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
